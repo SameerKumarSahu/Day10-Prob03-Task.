ade46904c07d981f0eecb819be6375433b82c2c2
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-29 22:59:45 +0530

    Sorting

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -175,3 +175,155 @@ echo "Percentage of THH occuring is " $k "%"
 echo "Percentage of THT occuring is " $l "%"
 echo "Percentage of TTH occuring is " $m "%"
 echo "Percentage of TTT occuring is " $n "%"
+
+
+
+
+counter=1
+
+H=0
+T=0
+HH=0
+HT=0
+TH=0
+TT=0
+HHH=0
+HHT=0
+HTH=0
+THH=0
+HTT=0
+THT=0
+TTH=0
+TTT=0
+for (( i=1; i<=150; i++ ))
+do
+ dice1=$(( RANDOM%13 ))
+
+        if [ $dice1 -eq 0 ]
+
+        then
+                ((H++))
+        elif [ $dice1 -eq 1 ]
+
+        then
+
+                 ((T++))
+
+        elif [ $dice1 -eq 2 ]
+
+
+        then
+                 ((HH++))
+
+        elif [ $dice1 -eq 3 ]
+        then
+                ((HT++))
+
+        elif [ $dice1 -eq 4 ]
+
+
+        then
+
+                ((TH++))
+
+        elif [ $dice1 -eq 5 ]
+
+        then
+                ((TT++))
+
+        elif [ $dice1 -eq 6 ]
+
+
+        then
+
+                ((HHH++))
+        elif [ $dice1 -eq 7 ]
+
+
+        then
+                ((HHT++))
+
+        elif [ $dice1 -eq 8 ]
+
+
+        then
+
+                ((HTH++))
+
+        elif [ $dice1 -eq 9 ]
+
+
+        then
+                 ((THH++))
+
+        elif [ $dice1 -eq 10 ]
+
+
+        then
+                 ((THT++))
+        elif [ $dice1 -eq 11 ]
+
+
+        then
+
+                ((TTH++))
+
+        elif [ $dice1 -eq 12 ]
+
+
+        then
+
+                ((TTT++))
+
+        else
+
+               ((HTT++))
+
+        fi
+done
+
+
+
+
+
+result[((counter++))]=$H
+result[((counter++))]=$T
+result[((counter++))]=$HH
+result[((counter++))]=$HT
+result[((counter++))]=$TH
+result[((counter++))]=$TT
+result[((counter++))]=$HHH
+result[((counter++))]=$HHT
+result[((counter++))]=$HTH
+result[((counter++))]=$HTT
+result[((counter++))]=$THH
+result[((counter++))]=$THT
+result[((counter++))]=$TTH
+result[((counter++))]=$TTT
+
+
+echo "${result[@]}"
+
+
+
+sort=0
+
+for ((i=1; i<=14; i++))
+do
+    for((j=1; j<=14; j++))
+    do
+        if [ ${result[i]} -lt ${result[j]} ]
+        then
+                continue
+         elif [ ${result[i]} -eq ${result[j]} ]
+        then
+                continue
+        else
+            sort=${result[i]}
+            result[i]=${result[j]}
+            result[j]=$sort
+        fi
+    done
+done
+
+echo "${result[@]}"
