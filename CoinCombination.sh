e40eccdfdd38bb7867593cde58b72913fc755476
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-29 22:15:41 +0530

    Singlet

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -11,3 +11,26 @@ then
 else
 	echo "TAIL"
 fi
+
+declare -A Dict
+H=0
+T=0
+for (( i=1; i<=10; i++ ))
+do
+Flipcoin=$((RANDOM%2))
+
+if [ $Flipcoin -eq 0 ]
+then
+        Dict[Face1]="HEAD"
+	((H++))
+else
+        Dict[Face2]="TAIL"
+	((T++))
+fi
+done
+echo ${Dict[@]}
+a=$(($H * 10))
+b=$(($T * 10))
+echo "Percentage of Head occuring is " $a "%"
+echo "Percentage of is Tail occuring " $b "%"
+
